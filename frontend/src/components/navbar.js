c93ed2c7f218ed2c439880771587bfdb43621d02
cassandra18 <cassandralelei013@gmail.com>
2023-12-19 14:12:41 +0300

    created the toggle menu

--- frontend/src/components/navbar.js
+++ frontend/src/components/navbar.js
@@ -1,7 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import bookshopLogo from "../assets/images/bookshop_logo.jpeg";
 
 const Navbar = () => {
+  const [showMenu, setShowMenu] = useState(false);
+
+  const toggleMenu = () => {
+    setShowMenu(!showMenu);
+  };
+
+
   return (
     <div className="navbar-container">
       <div className="left-section">
@@ -17,6 +24,14 @@ const Navbar = () => {
       <div className="right-section">
         <div className="links-wrapper">
           <a href="/" className="home-link">Home</a>
+          <button className="menu-btn" onClick={toggleMenu}>Menu</button>
+          <div className={`hidden-links ${showMenu ? 'show-links': ""}`}>
+            <a href="/lelann-stores" className="lelann-stores">Lelann stores</a>
+            <a href="/contact" className="contact">Contact</a>
+            <a href="/about-us" className="about-us">About Us</a>
+            <a href="/services" className="services">Our services</a>
+            <a href="/help" className="help-shopping-online">Need help shopping online?</a>
+          </div>
         </div>
       </div>
     </div>
