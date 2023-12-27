4e9d3fa9c434e9a36a306c271434b287953844d8
cassandra18 <cassandralelei013@gmail.com>
2023-12-27 12:15:38 +0300

    complited coding the functionality of the search bar

--- frontend/src/app.jsx
+++ frontend/src/app.jsx
@@ -1,13 +1,19 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 
 import Navbar from './components/navbar';
 import SearchBar from './components/searchBar';
+import SearchResultsList from './components/searchResultsList';
+
+
 const App = () => {
+
+    const [ results, setResults ] = useState([]);
     return (
         <Router>
                <Navbar/>
-               <SearchBar/>
+               <SearchBar setResults={setResults} />
+               <SearchResultsList results={results} />
         </Router>
     );
 };
